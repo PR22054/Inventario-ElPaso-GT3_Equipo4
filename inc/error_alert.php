dcf19edb16fd673a3011693aef48388aed97a5b7
PR22054 <pr22054@ues.edu.sv>
2025-05-20 19:53:21 -0600

    Add files via upload
    Archivos del proyecto hechos conforme el los PBI del Sprint 1

---
+++ inc/error_alert.php
@@ -0,0 +1,4 @@
+<div class="notification is-danger is-light mb-6 mt-6">
+    <strong>¡Ocurrio un error, intente mas tarde</strong><br>
+    No podemos obtener la información 
+</div>
---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+<div class="notification is-success is-light pb-6 pt-6" style="font-size: 20px;">
+Taller el Paso. 2025
+<br><br>
+Sistema de Inventarios
+</div>
