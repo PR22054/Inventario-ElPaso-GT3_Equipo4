7fa5488442b9067fddea0b18c4f3ddcda49fecc2
PR22054 <pr22054@ues.edu.sv>
2025-09-15 21:51:55 -0600

    Delete inc directory

--- inc/head.php
+++
@@ -1,5 +0,0 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>DIS115 Proyecto</title>
-<link rel="stylesheet" href="./css/bulma.min.css">
-<link rel="stylesheet" href="./css/estilos.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
